f0d95e12eb80d238c12a060a7fb7a13f55eb23e8
Adriana Picoral <adriana@Adrianas-MacBook-Pro.local>
2020-08-15 11:05:06 -0700

    updated website

---
+++ src/sada_main.js
@@ -0,0 +1,22 @@
+//'use strict'
+
+var lightTheme = {
+  mainBackground: "bg-white",
+  altBackground: "bg-light",
+  altText: "text-secondary",
+  mainText: "text-dark",
+  linkText: "text-success",
+  skillBackground: "bg-success"
+};
+
+var darkTheme = {
+  mainBackground: "bg-dark",
+  altBackground: "bg-secondary",
+  altText: "text-light",
+  mainText: "text-white",
+  linkText: "text-warning",
+  altLink: "text-info",
+  skillBackground: "bg-warning"
+};
+
+var isDark = true;
